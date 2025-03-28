9890a00cec69850e7d13ad55769394797c48a816
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-27 20:05:06 -0400

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -9,7 +9,6 @@ if (global.conn.user.jid !== conn.user.jid) {
 return conn.reply(m.chat, '☯︎ *Utiliza este comando directamente en el número principal del Bot*', m, )
 }
 await conn.reply(m.chat, '🂱 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, )
-m.react(rwait)
 
 let sessionPath = './Blacksesion/'
 
